4c68f55c45c4874e6849d9e935ef1cb258c73196
[Andrew Core] <[adcore89@gmail.com]>
2021-04-27 21:37:46 -0400

    part way thru module

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const { animals } = require('./data/animals');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+//parse incoming string
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -36,6 +43,16 @@ function findById(id, animalsArray) {
   return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+const animal = body;
+animalsArray.push(animal);
+fs.writeFileSync(
+  path.join(__dirname, './data/animals.json'),
+  JSON.stringify({ animalsArray }, null, 2)
+);
+return animal;
+}
+
 app.get('/api/animals', (req, res) => {
   let results = animals;
   if (req.query) {
@@ -51,6 +68,11 @@ app.get('/api/animals/:id', (req, res) => {
   } else {
     res.send(404);
   }
+});
+
+app.post('/api/animals', (req,res) => { 
+  console.log(req.body);
+  res.json(req.body);
 });
 
 app.listen(PORT, () => {
